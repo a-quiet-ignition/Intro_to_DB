b863c610d6a2b37a838ce6ce0457d42c111e06bd
a-quiet-ignition <obatolaraymond@gmail.com>
2025-10-19 21:23:09 +0100

    add alx_book_store.sql file

---
+++ alx_book_store.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE alx_book_store;
+
+CREATE TABLE IF NOT EXISTS books (
+  book_id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(130) NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES authors(author_id),
+  price DOUBLE,
+  publication_date DATE
+);
+
+CREATE TABLE IF NOT EXISTS authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE IF NOT EXISTS customers (
+  customer_id INT AUTO_INCREMENT PRIMARY KEY,
+  customer_name VARCHAR(215) NOT NULL,
+  email VARCHAR(215),
+  address TEXT
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+  order_id INT AUTO_INCREMENT PRIMARY KEY,
+  FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+  order_date DATE
+);
+
+CREATE TABLE IF NOT EXISTS order_details (
+  order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
+  FOREIGN KEY (order_id) REFERENCES orders(order_id),
+  FOREIGN KEY (book_id) REFERENCES books(book_id),
+  quantity DOUBLE,
+);
