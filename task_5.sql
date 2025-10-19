ffd04251dbebde4d3bdcf2a388eb3735830d9091
a-quiet-ignition <obatolaraymond@gmail.com>
2025-10-19 23:06:24 +0100

    add task_5.sql file

---
+++ task_5.sql
@@ -0,0 +1,3 @@
+["INFORMATION_SCHEMA.COLUMNS", "COLUMN_NAME", "COLUMN_TYPE", "TABLE_SCHEMA = 'alx_book_store'", "TABLE_NAME = 'Books'"];
+
+INSERT INTO Customers (customer_id = 1, customer_name = 'Cole Baidoo', email = 'cbaidoo@sandtech.com', address = '123 Happiness Ave');
