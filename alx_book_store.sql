c6c09ff79e72b0048b9436a159b05f3bf7798afe
a-quiet-ignition <obatolaraymond@gmail.com>
2025-10-19 21:34:34 +0100

    update foreign keys table names in alx_book_store.sql file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 CREATE TABLE IF NOT EXISTS Books (
   book_id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(130) NOT NULL,
-  FOREIGN KEY (author_id) REFERENCES authors(author_id),
+  FOREIGN KEY (author_id) REFERENCES Authors(author_id),
   price DOUBLE,
   publication_date DATE
 );
@@ -22,13 +22,13 @@ CREATE TABLE IF NOT EXISTS Customers (
 
 CREATE TABLE IF NOT EXISTS Orders (
   order_id INT AUTO_INCREMENT PRIMARY KEY,
-  FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
   order_date DATE
 );
 
 CREATE TABLE IF NOT EXISTS Order_details (
   order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
-  FOREIGN KEY (order_id) REFERENCES orders(order_id),
-  FOREIGN KEY (book_id) REFERENCES books(book_id),
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+  FOREIGN KEY (book_id) REFERENCES Books(book_id),
   quantity DOUBLE,
 );
