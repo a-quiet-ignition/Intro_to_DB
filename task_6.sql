587a23ee98562675525ea403c7d067ef1f34444c
a-quiet-ignition <obatolaraymond@gmail.com>
2025-10-19 23:23:03 +0100

    add task_6.sql file

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,5 @@
 ["INFORMATION_SCHEMA.COLUMNS", "COLUMN_NAME", "COLUMN_TYPE", "TABLE_SCHEMA = 'alx_book_store'", "TABLE_NAME = 'Books'"];
 
-INSERT INTO customer 
-VALUES (customer_id = 2, customer_name = 'Blessing Malik', email = 'bmalik@sandtech.com', address = "124 Happiness Ave."),
+INSERT INTO customer VALUES (customer_id = 2, customer_name = 'Blessing Malik', email = 'bmalik@sandtech.com', address = "124 Happiness Ave."),
        (customer_id = 3, customer_name = 'Obed Ehoneah', email = 'eobed@sandtech.com', address = '125 Happiness Ave.'),
        (customer_id = 4, customer_name = 'Nehemial Kamolu', email = 'nkamolu@sandtech.com', address = '126 Happiness Ave.');
