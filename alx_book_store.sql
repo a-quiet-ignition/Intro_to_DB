f5b74d5da95ae4565bb8377388fc4dad6700e20f
a-quiet-ignition <obatolaraymond@gmail.com>
2025-10-19 21:32:11 +0100

    update table names in alx_book_store.sql file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS Books (
   book_id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(130) NOT NULL,
   FOREIGN KEY (author_id) REFERENCES authors(author_id),
@@ -8,25 +8,25 @@ CREATE TABLE IF NOT EXISTS books (
   publication_date DATE
 );
 
-CREATE TABLE IF NOT EXISTS authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS Customers (
   customer_id INT AUTO_INCREMENT PRIMARY KEY,
   customer_name VARCHAR(215) NOT NULL,
   email VARCHAR(215),
   address TEXT
 );
 
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
   order_id INT AUTO_INCREMENT PRIMARY KEY,
   FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
   order_date DATE
 );
 
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_details (
   order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
   FOREIGN KEY (order_id) REFERENCES orders(order_id),
   FOREIGN KEY (book_id) REFERENCES books(book_id),
